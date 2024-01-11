d473d956adb3da36553b9f562e1e99c44795fc49
minecraft-Rayson <minecraft-Rayson@outlook.com>
2024-01-11 08:37:33 +0800

    更新 抽奖2.py

--- 抽奖2.py
+++ 抽奖2.py
@@ -1,35 +1,35 @@
 ﻿from tkinter import *
 import random,time
 def choujiang():
-    global num1
+    global num1,lbl
     num1=random.randint(1,5)
     if num1==1:
-        for i in range(1,6):
-            time.sleep(2)
-            print(i)
-        print("奖品1")
+        lbl = Label(root, text="奖品1",font=("仿宋", 50))
+        lbl.pack()
+        time.sleep(3)
+        lbl.forget()
     elif num1==2:
-        for i in range(1,6):
-            time.sleep(2)
-            print(i)
-        print("奖品2")
+        lbl = Label(root, text="奖品2",font=("仿宋", 50))
+        lbl.pack()
+        time.sleep(3)
+        lbl.forget()
     elif num1==3:
-        for i in range(1,6):
-            time.sleep(2)
-            print(i)
-        print("奖品3")
+        lbl = Label(root, text="奖品3",font=("仿宋", 50))
+        lbl.pack()
+        time.sleep(3)
+        lbl.forget()
     elif num1==4:
-        for i in range(1,6):
-            time.sleep(2)
-            print(i)
-        print("奖品4")
+        lbl = Label(root, text="奖品4",font=("仿宋", 50))
+        lbl.pack()
+        time.sleep(3)
+        lbl.forget()
     elif num1==5:
-        for i in range(1,6):
-            time.sleep(2)
-            print(i)
-        print("奖品5")
+        lbl = Label(root, text="奖品5",font=("仿宋", 50))
+        lbl.pack()
+        time.sleep(3)
+        lbl.forget()
 root = Tk()
-root.geometry("162x59+374+182")
+root.geometry("500x500+374+182")
 root.title("666")
 button = Button(root,text="按下抽奖",font=("仿宋",25),fg="blue",command=choujiang)
 button.grid(row=1,column=1)
